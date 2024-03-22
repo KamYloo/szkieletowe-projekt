c82b522ebcecc1ca452ef9a57a718ab5ae47db25
Gabriel Charkiewicz <108090061+kejden@users.noreply.github.com>
2024-03-22 22:04:31 +0100

    Prosty template profilu (wystarczy podmienic potem na lepszy), Edytowanie profilu

--- mysite/users/models.py
+++ mysite/users/models.py
@@ -1,9 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
-class Person(models.Model):
+class Profile(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
-	profile_pic = models.ImageField(default="default.png")
+	profile_pic = models.ImageField(default="default.png", upload_to="profile_pics")
 
 	def __str__(self):
-		return f"{self.name} {self.lastname}"
+		return f"{self.user.first_name} {self.user.last_name} Profile"
+
+	def save(self):
+		super().save()
+
+		img = Image.open(self.profile_pic.path)
+
+		if img.height > 256 or img.width > 256:
+			output_size = (256, 256)
+			img.thumbnail(output_size)
+			img.save(self.profile_pic.path)
